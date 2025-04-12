ca5148daf58182f884c5f8a7498bc43d1024e644
Johannes Gaber <jgaber@mit.edu>
2025-04-12 18:51:45 -0400

    cluster push

--- RL/src/helperfunctions/LRRParser.py
+++ RL/src/helperfunctions/LRRParser.py
@@ -6,17 +6,28 @@ class LRRParser:
     def __init__(self, env, cfg):
         self.input_size = cfg.input_size
         self.nNodes = env.nNodes
+        self.normalise_obs = cfg.normalise_obs
         self.agent_scale_fac = 1/math.ceil(env.nAgents/env.nNodes*4)
         self.task_scale_fac = 1/math.ceil(env.nTasks/env.nNodes*4)
         self.edge_index = self.get_edge_index(env)
         
         
     def parse_obs(self, obs):
-        x = torch.cat((
-            torch.clip(torch.tensor(obs["agents_per_node"])*self.agent_scale_fac, 0, 1).view(1, self.nNodes).float(),
-            torch.clip(torch.tensor(obs["free_agents_per_node"])*self.agent_scale_fac, 0, 1).view(1, self.nNodes).float(),
-            torch.clip(torch.tensor(obs["free_tasks_per_node"])*self.task_scale_fac, 0, 1).view(1, self.nNodes).float()
+        if self.normalise_obs:
+            x = torch.cat((
+                torch.clip(torch.tensor(obs["agents_per_node"])*self.agent_scale_fac, 0, 1).view(1, self.nNodes).float(),
+                torch.clip(torch.tensor(obs["free_agents_per_node"])*self.agent_scale_fac, 0, 1).view(1, self.nNodes).float(),
+                torch.clip(torch.tensor(obs["free_tasks_per_node"])*self.task_scale_fac, 0, 1).view(1, self.nNodes).float()
+            )
+            
+            ,dim=0).view(self.input_size, self.nNodes).T
+        else:
+            x = torch.cat((
+            torch.tensor(obs["agents_per_node"]).view(1, self.nNodes).float(),
+            torch.tensor(obs["free_agents_per_node"]).view(1, self.nNodes).float(),
+            torch.tensor(obs["free_tasks_per_node"]).view(1, self.nNodes).float()
         )
+        
         ,dim=0).view(self.input_size, self.nNodes).T
 
         data = Data(x, self.edge_index)
