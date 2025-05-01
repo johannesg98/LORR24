f283c9cc6e1a5bc067f540c9b9d534a7273f80a1
Johannes Gaber <jgaber@mit.edu>
2025-04-30 23:00:12 -0400

    cluster push

--- nn_optimization/nn_test.py
+++ nn_optimization/nn_test.py
@@ -15,7 +15,7 @@ import sys
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(script_dir)
 
-from nets.penta_edge_index import GNNActor
+from nets.penta_cat import GNNActor
 
 # Device configuration (GPU if available, otherwise CPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -288,7 +288,7 @@ for i in range(n_experiments):
     wandb_dict = {
         "project": "nn-sparse-grid-search",
     }
-    name = "penta_edge_index_reuse_layer"
+    name = "penta_cat_fixed_layers"
         
     match i:
         case 0:
