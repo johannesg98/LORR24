9a16c9d38c347bd015f20093b0f89aaeea3964f5
Johannes Gaber <jgaber@mit.edu>
2025-05-06 15:33:49 -0400

    cluster push

--- nn_optimization/nn_test_NNConv.py
+++ nn_optimization/nn_test_NNConv.py
@@ -293,7 +293,7 @@ for i in range(n_experiments):
     wandb_dict = {
         "project": "nn-sparse-grid-search",
     }
-    name = "NNConv_1.0"
+    name = "NNConv_0.5_cpu"
         
     match i:
         case 0:
