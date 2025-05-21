7fb3cead5ea97025f3bd8b512057e225d79db977
Johannes Gaber <jgaber@mit.edu>
2025-05-20 22:31:26 -0400

    cluster push

--- RL/src/nets/TransformerConv.py
+++ RL/src/nets/TransformerConv.py
@@ -7,33 +7,22 @@ from torch_geometric.data import Data, Batch
 import numpy as np
 import os
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
 class GNNActor(nn.Module):
     def __init__(self, in_channels, hidden_size=32, act_dim=6, edge_limit=0.5, out_channel_fac=2, edge_feature_dim=6):
         super().__init__()
-        # self.edge_limit = edge_limit
-        # self.pos_dim = 6
-        # in_channels += self.pos_dim
         self.in_channels = in_channels
         self.out_channels = out_channel_fac * in_channels
         self.act_dim = act_dim
-        # self.conv1 = NNConv(in_channels, self.out_channels, nn=nn.Sequential(nn.Linear(1,16), nn.ReLU(), nn.Linear(16,in_channels*self.out_channels)))
         self.conv1 = TransformerConv(in_channels, self.out_channels, edge_dim=edge_feature_dim)
         self.lin1 = nn.Linear(in_channels+self.out_channels, hidden_size)
         self.lin2 = nn.Linear(hidden_size, hidden_size)
         self.lin3 = nn.Linear(hidden_size, 1)
         self.lin1_norm = nn.LayerNorm(hidden_size)
         self.lin2_norm = nn.LayerNorm(hidden_size)
-        # self.pos_feat = self.get_positions()
-        # self.edge_weights, self.edge_index_distancebased = self.get_edge_weights()
-        # self.max_storage = np.empty(0)
-
+        
 
 
     def forward(self, state, edge_index, edge_attr, deterministic=False, return_dist=False, return_raw=False):
-        # deterministic=True
         
         out1 = F.relu(self.conv1(state, edge_index, edge_attr=edge_attr))
 
@@ -60,50 +49,7 @@ class GNNActor(nn.Module):
             log_prob = m.log_prob(action)
         return action, log_prob
     
-    # def get_positions(self):
-    #     pos_indices = [120,124,128,132,136,140,144,148,152,237,241,245,249,253,257,261,265,269,354,358,362,366,370,374,378,382,386,471,475,479,483,487,491,495,499,503,588,592,596,600,604,608,612,616,620,705,709,713,717,721,725,729,733,737,822,826,830,834,838,842,846,850,854,48,53,60,67,73,157,352,388,583,586,817,901,906,913,920,926]
-    #     height = 25
-    #     width = 39
-    #     nNodesss = 79
-    #     position_features = torch.zeros((nNodesss,self.pos_dim))
-    #     for i,pos_idx in enumerate(pos_indices):
-    #         x = pos_idx % width
-    #         y = pos_idx // width
-    #         x_norm = x / (width-1)
-    #         y_norm = y / (height-1)
-    #         position_features[i,0] = x_norm
-    #         position_features[i,1] = y_norm
-    #         position_features[i,2] = (np.sin(x_norm*2*np.pi)+1)/2
-    #         position_features[i,3] = (np.cos(x_norm*2*np.pi)+1)/2
-    #         position_features[i,4] = (np.sin(y_norm*2*np.pi)+1)/2
-    #         position_features[i,5] = (np.cos(y_norm*2*np.pi)+1)/2
-    #     return position_features.to(device)
-    
 
-    # def get_edge_weights(self):
-    #     NodeCostMatrix = torch.load(os.path.join(script_dir, "../../../nn_optimization/data/NodeCostMatrix.pt"))
-    #     nNodesss = NodeCostMatrix.shape[0]
-    #     print("NodeCostMatrix Nodes: ", nNodesss)
-
-    #     NodeCostMatrix = NodeCostMatrix/NodeCostMatrix.max()
-    #     origin = []
-    #     destination = []
-    #     weight = []
-    #     for o in range(nNodesss):
-    #       for d in range(nNodesss):
-    #         if NodeCostMatrix[o][d] < self.edge_limit:
-    #           origin.append(o)
-    #           destination.append(d)
-    #           weight.append(NodeCostMatrix[o][d])
-
-    #     edge_index_distancebased = torch.cat([torch.tensor([origin]), torch.tensor([destination])])
-    #     weights = torch.tensor([weight])
-
-    #     weights = (self.edge_limit-weights) / self.edge_limit
-
-    #     return weights.squeeze(0).unsqueeze(-1).to(device), edge_index_distancebased.to(device)
-
-    
 
 
 class GNNCritic(nn.Module):
@@ -113,9 +59,6 @@ class GNNCritic(nn.Module):
 
     def __init__(self, in_channels, hidden_size=32, act_dim=6, edge_limit=0.5, out_channel_fac=2, edge_feature_dim=6):
         super().__init__()
-        # self.edge_limit = edge_limit
-        # self.pos_dim = 6
-        # in_channels += self.pos_dim
         self.in_channels = in_channels
         self.out_channels = out_channel_fac * in_channels
         self.act_dim = act_dim
@@ -125,8 +68,6 @@ class GNNCritic(nn.Module):
         self.lin3 = nn.Linear(hidden_size, 1)
         self.lin1_norm = nn.LayerNorm(hidden_size)
         self.lin2_norm = nn.LayerNorm(hidden_size)
-        # self.pos_feat = self.get_positions()
-        # self.edge_weights, self.edge_index_distancebased = self.get_edge_weights()
 
     def forward(self, state, edge_index, edge_attr, action):
         
@@ -143,48 +84,3 @@ class GNNCritic(nn.Module):
         x = self.lin3(x).squeeze(-1)  # (B)
         return x
     
-
-
-
-    # def get_positions(self):
-    #     pos_indices = [120,124,128,132,136,140,144,148,152,237,241,245,249,253,257,261,265,269,354,358,362,366,370,374,378,382,386,471,475,479,483,487,491,495,499,503,588,592,596,600,604,608,612,616,620,705,709,713,717,721,725,729,733,737,822,826,830,834,838,842,846,850,854,48,53,60,67,73,157,352,388,583,586,817,901,906,913,920,926]
-    #     height = 25
-    #     width = 39
-    #     nNodesss = 79
-    #     position_features = torch.zeros((nNodesss,self.pos_dim))
-    #     for i,pos_idx in enumerate(pos_indices):
-    #         x = pos_idx % width
-    #         y = pos_idx // width
-    #         x_norm = x / (width-1)
-    #         y_norm = y / (height-1)
-    #         position_features[i,0] = x_norm
-    #         position_features[i,1] = y_norm
-    #         position_features[i,2] = (np.sin(x_norm*2*np.pi)+1)/2
-    #         position_features[i,3] = (np.cos(x_norm*2*np.pi)+1)/2
-    #         position_features[i,4] = (np.sin(y_norm*2*np.pi)+1)/2
-    #         position_features[i,5] = (np.cos(y_norm*2*np.pi)+1)/2
-    #     return position_features.to(device)
-    
-
-    # def get_edge_weights(self):
-    #     NodeCostMatrix = torch.load(os.path.join(script_dir, "../../../nn_optimization/data/NodeCostMatrix.pt"))
-    #     nNodesss = NodeCostMatrix.shape[0]
-    #     print("NodeCostMatrix Nodes: ", nNodesss)
-
-    #     NodeCostMatrix = NodeCostMatrix/NodeCostMatrix.max()
-    #     origin = []
-    #     destination = []
-    #     weight = []
-    #     for o in range(nNodesss):
-    #       for d in range(nNodesss):
-    #         if NodeCostMatrix[o][d] < self.edge_limit:
-    #           origin.append(o)
-    #           destination.append(d)
-    #           weight.append(NodeCostMatrix[o][d])
-
-    #     edge_index_distancebased = torch.cat([torch.tensor([origin]), torch.tensor([destination])])
-    #     weights = torch.tensor([weight])
-
-    #     weights = (self.edge_limit-weights) / self.edge_limit
-
-        return weights.squeeze(0).unsqueeze(-1).to(device), edge_index_distancebased.to(device)
