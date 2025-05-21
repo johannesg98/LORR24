640bb51ce563d55b0160879b6ca62ccb1e9f34f1
Johannes Gaber <jgaber@mit.edu>
2025-05-21 00:15:10 -0400

    cluster push

--- RL/src/algos/sac.py
+++ RL/src/algos/sac.py
@@ -718,8 +718,7 @@ class SAC(nn.Module):
 
                 print("New Obs x device: ", new_obs_parsed.x.device, " edge_index device: ", new_obs_parsed.edge_index.device, " edge_attr device: ", new_obs_parsed.edge_attr.device)
                 print("Obs x device: ", obs_parsed.x.device, " edge_index device: ", obs_parsed.edge_index.device, " edge_attr device: ", obs_parsed.edge_attr.device)
-                print("Action device: ", action_rl.device)
-                print("Reward device: ", rew.device)
+                
                 
                 # learn
                 if cfg.model.train and i_episode > cfg.model.start_training_at_episode:
