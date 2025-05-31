12539895caf61009aa59e3f253be7300aa9fd49e
Johannes Gaber <jgaber@mit.edu>
2025-05-31 03:41:03 -0400

    cluster push

--- roadmapRL/train.py
+++ roadmapRL/train.py
@@ -40,7 +40,7 @@ def main(cfg: DictConfig):
         observationTypes={"roadmap-activation"},
         random_agents_and_tasks="true",
         use_dummy_goals_for_idle_agents=cfg.model.use_dummy_goals_for_idle_agents,
-        scheduler_type="ILP",
+        scheduler_type="default",
         planner_type="default",
         guarantee_planner_time = True
     )
