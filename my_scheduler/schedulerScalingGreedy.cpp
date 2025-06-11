61bf87acce8b82d90a49ed23f6f49e2d71c30ec9
Johannes Gaber <jgaber@mit.edu>
2025-06-10 23:43:54 -0400

    cluster push

--- my_scheduler/schedulerScalingGreedy.cpp
+++ my_scheduler/schedulerScalingGreedy.cpp
@@ -77,12 +77,7 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
             c_loc = env->curr_states.at(i).location;
             // dist = DefaultPlanner::get_h(env, c_loc, env->task_pool[t_id].locations[0]);
 
-            if (scaling_active){
-                a_node = env->nodes->regions.at(c_loc);
-                t_node = env->nodes->regions.at(env->task_pool[t_id].locations[0]);
-                // dist = dist * (scaling_action[a_node][0]+0.5) * (scaling_action[t_node][1]+0.5);
-                dist = dist * (2.7*scaling_action[a_node][0]+0.3) * (2.7*scaling_action[t_node][1]+0.3);
-            }
+            
 
 
             dist = 0;
@@ -91,6 +86,13 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
             for (int loc : env->task_pool[t_id].locations){
                 dist += DefaultPlanner::get_h(env, c_loc, loc);
                 c_loc = loc;
+            }
+
+            if (scaling_active){
+                a_node = env->nodes->regions.at(c_loc);
+                t_node = env->nodes->regions.at(env->task_pool[t_id].locations[0]);
+                dist = dist * (scaling_action[a_node][0]+0.5) * (scaling_action[t_node][1]+0.5);
+                // dist = dist * (2.7*scaling_action[a_node][0]+0.3) * (2.7*scaling_action[t_node][1]+0.3);
             }
             
 
